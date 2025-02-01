a0bf6770705755fb9d91e9faf477a48407abe22b
rachaz <rachaz@naver.com>
2025-02-01 23:08:46 +0900

    250201

---
+++ Data_analytics/250130/P. 문자열 겹쳐쓰기.py
@@ -0,0 +1,11 @@
+# 문제 설명
+# 1. 문자열 my_string, overwrite_string과 정수 s가 주어집니다. 
+# 문자열 my_string의 인덱스 s부터 overwrite_string의 길이만큼을 
+# 문자열 overwrite_string으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+# 1) my_string의 특정 위치 s부터
+# 2) overwrite_string 의 길이만큼을
+# 3) overwrite_string으로 덮어쓰기 하는 문제
+
+# 즉, my_string의 s번째 위치부터 overwrite_string의 길이만큼을 덮어쓴 결과를 반환하면 된다!
+
